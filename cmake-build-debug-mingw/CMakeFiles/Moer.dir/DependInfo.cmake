c04d5a17dae572cb1a0daa7cff8a2da082a4ed27
PlutoEr-001 <18734458689@qq.com>
2023-07-31 20:14:24 +0800

    :rocket: finish final task

---
+++ cmake-build-debug-mingw/CMakeFiles/Moer.dir/DependInfo.cmake
@@ -0,0 +1,62 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/CoreLayer/Math/Transform.cpp" "CMakeFiles/Moer.dir/src/CoreLayer/Math/Transform.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/CoreLayer/Math/Transform.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Acceleration/AABB.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/AABB.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/AABB.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Acceleration/Acceleration.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/Acceleration.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/Acceleration.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Acceleration/BVH.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/BVH.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/BVH.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Acceleration/EmbreeBVH.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/EmbreeBVH.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/EmbreeBVH.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Acceleration/Linear.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/Linear.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/Linear.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Acceleration/Octree.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/Octree.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Acceleration/Octree.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Camera/Camera.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Camera/Camera.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Camera/Camera.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Camera/Pinhole.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Camera/Pinhole.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Camera/Pinhole.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Film/Film.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Film/Film.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Film/Film.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Filter/Filter.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Filter/Filter.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Filter/Filter.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Integrator/DirectIntegrator.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/DirectIntegrator.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/DirectIntegrator.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Integrator/NormalIntegrator.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/NormalIntegrator.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/NormalIntegrator.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Integrator/PathIntegrator.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/PathIntegrator.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/PathIntegrator.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Integrator/UVIntegrator.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/UVIntegrator.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/UVIntegrator.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Integrator/WhittedIntegrator.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/WhittedIntegrator.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Integrator/WhittedIntegrator.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Light/AreaLight.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Light/AreaLight.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Light/AreaLight.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Light/EnvironmentLight.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Light/EnvironmentLight.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Light/EnvironmentLight.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Light/SpotLight.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Light/SpotLight.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Light/SpotLight.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Material/Conductor.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Conductor.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Conductor.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Material/Dielectric.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Dielectric.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Dielectric.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Material/Material.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Material.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Material.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Material/Matte.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Matte.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Matte.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Material/Mirror.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Mirror.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Mirror.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Material/OrenNayar.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/OrenNayar.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/OrenNayar.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Material/Phong.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Phong.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Material/Phong.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Sampler/IndependentSampler.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Sampler/IndependentSampler.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Sampler/IndependentSampler.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Scene/Scene.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Scene/Scene.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Scene/Scene.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Shape/Cone.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Cone.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Cone.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Shape/Cube.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Cube.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Cube.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Shape/Cylinder.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Cylinder.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Cylinder.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Shape/Disk.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Disk.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Disk.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Shape/Parallelogram.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Parallelogram.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Parallelogram.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Shape/Shape.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Shape.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Shape.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Shape/Sphere.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Sphere.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Sphere.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Shape/Triangle.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Triangle.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Shape/Triangle.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Texture/ImageTexture.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Texture/ImageTexture.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Texture/ImageTexture.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Texture/Mipmap.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Texture/Mipmap.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Texture/Mipmap.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Texture/NormalTexture.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Texture/NormalTexture.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Texture/NormalTexture.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/FunctionLayer/Texture/Texture.cpp" "CMakeFiles/Moer.dir/src/FunctionLayer/Texture/Texture.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/FunctionLayer/Texture/Texture.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/ResourceLayer/FileUtil.cpp" "CMakeFiles/Moer.dir/src/ResourceLayer/FileUtil.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/ResourceLayer/FileUtil.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/ResourceLayer/Image.cpp" "CMakeFiles/Moer.dir/src/ResourceLayer/Image.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/ResourceLayer/Image.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/ResourceLayer/Mesh.cpp" "CMakeFiles/Moer.dir/src/ResourceLayer/Mesh.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/ResourceLayer/Mesh.cpp.obj.d"
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/src/main.cpp" "CMakeFiles/Moer.dir/src/main.cpp.obj" "gcc" "CMakeFiles/Moer.dir/src/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug-mingw/externals/tinyobjloader/CMakeFiles/tinyobjloader.dir/cmake_clean_target.cmake
@@ -0,0 +1,3 @@
+file(REMOVE_RECURSE
+  "C:/Users/plutoEr/CLionProjects/Moer-lite/target/lib/libtinyobjloader.a"
+)
